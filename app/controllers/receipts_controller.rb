222a5e83b9f692014dcb085e4cb510397383fba0
q <qpham415@gmail.com>
2015-03-25 18:34:02 -0700

    receipt stylesheet and controller update

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,2 +1,37 @@
 class ReceiptsController < ApplicationController
+  def welcome
+  end
+
+  def index
+    @receipts = Receipt.all
+
+  end
+
+  def show
+    @receipt = Receipt.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @receipt = Receipt.new(receipt_params)
+    @receipt.save
+    redirect_to @receipt
+  end
+
+  def update
+  end
+
+  def destroy
+    @receipt = Receipt.find(params[:id])
+    @receipt.destroy
+    redirect_to receipts_path
+  end
+
+  private
+    def receipt_params
+      params.require(:receipt).permit!
+    end
+
 end
