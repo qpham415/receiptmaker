016cb2dfcad13f6fd7773f9fb26afb95ffea167a
q <qpham415@gmail.com>
2015-04-06 14:54:45 -0700

    add assets pipeline to heroku

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Receiptmaker
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #Load assets on heroku
+    config.serve_static_assets = true
   end
 end
