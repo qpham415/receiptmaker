f107e762f1a4066f39e58bf925ff8f152eaa5bca
q <qpham415@gmail.com>
2015-04-07 12:01:58 -0700

    form validation

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,6 +1,15 @@
 class Receipt < ActiveRecord::Base
   has_many :line_items
   validates_presence_of :date, :from_address, :to_address
+  validate :date_is_date?
 
   accepts_nested_attributes_for :line_items, :allow_destroy => :true, reject_if: lambda {|attributes| attributes['qty'].blank?}
+
+  private
+    def date_is_date?
+      if !date.is_a?(Date)
+        errors.add(:date, 'must be a valid date')
+      end
+    end
+
 end
