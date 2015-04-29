200e4ba6ff0d9fad3e6cbe346d1262b3625be8b8
q <qpham415@gmail.com>
2015-04-28 21:48:01 -0700

    limit to local env

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
-config.merge! config.fetch(Rails.env, {})
-config.each do |key, value|
-  ENV[key] = value.to_s
+if Rails.env.development?
+  config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+  config.merge! config.fetch(Rails.env, {})
+  config.each do |key, value|
+    ENV[key] = value.to_s
+  end
 end
 
 module Receiptmaker
